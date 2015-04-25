956a477ebe7c4d2fcd8508a76d354aee0cf1bd47
Austin <geekthattweaks+git@gmail.com>
2015-04-24 20:34:53 -0500

    nodejs notes

---
+++ nodejs/test.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var filename = process.argv[2];
+
+console.log("about to read call file");
+
+console.log(process.argv);
+
+fs.readFile('test.js', function(err, data){
+	if (err){
+		console.log("unable to read file", "test.js", err);
+	} else {
+		console.log(data.toString());		
+	}
+});
+
+fs.readFile(filename, function(err, data){
+	if (err){
+		console.log("unable to read file", filename, err);
+	} else {
+		console.log(data.toString());		
+	}
+});
+
+console.log("after read file call");
